5b635dd45c8a2e7903f3189ea685bfb4be855343
Daniel Mejia <rdanielmejia7@gmail.com>
2019-06-15 22:55:15 -0600

    mate BD

--- app/src/main/java/dev/grupo5/autismotion/Numeros/MateBD.java
+++ app/src/main/java/dev/grupo5/autismotion/Numeros/MateBD.java
@@ -1,4 +1,32 @@
 package dev.grupo5.autismotion.Numeros;
 
-public class MateBD {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MateBD extends SQLiteOpenHelper {
+    private static MateBD singletonDBHelper;
+    public static final String DATABASE="numeros.db";
+    private static final int VERSION=1;
+
+    public static MateBD getSingleton(Context context) {
+        if (singletonDBHelper == null){
+            singletonDBHelper = new MateBD(context.getApplicationContext());
+        }
+
+        return singletonDBHelper;
+    }
+
+    private MateBD(Context context){
+        super(context,DATABASE,null,VERSION);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table imagenesnumeros ( id inetger not null, valor integer, nombre_imagen varchar(30), primary key (id) );");
+
+    }
+
 }
