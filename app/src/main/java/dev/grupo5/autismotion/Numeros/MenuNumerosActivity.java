de33e84c0b9bb9087faf3c31842d9f466640e1cd
ElmerHernandez <elmerh49@gmail.com>
2019-06-15 22:36:23 -0600

    Update

---
+++ app/src/main/java/dev/grupo5/autismotion/Numeros/MenuNumerosActivity.java
@@ -0,0 +1,14 @@
+package dev.grupo5.autismotion.Numeros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import dev.grupo5.autismotion.R;
+
+public class MenuNumerosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_numeros);
+    }
+}
