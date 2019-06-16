1ebf2e329c5ceb29e47661810a32793852b68241
ElmerHernandez <elmerh49@gmail.com>
2019-06-15 22:25:52 -0600

    Numeros

--- app/src/main/java/dev/grupo5/autismotion/Numeros/RestandoNumerosActivity.java
+++ app/src/main/java/dev/grupo5/autismotion/Numeros/RestandoNumerosActivity.java
@@ -1,15 +1,14 @@
-package dev.grupo5.autismotion;
+package dev.grupo5.autismotion.Numeros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import dev.grupo5.autismotion.R;
 
-public class MainActivity extends AppCompatActivity {
+public class RestandoNumerosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_restando_numeros);
     }
-
-
 }
