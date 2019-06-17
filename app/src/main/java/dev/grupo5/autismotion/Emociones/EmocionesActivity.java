5cbdbb7870a2c3844989b268c2bb9e6bd580b341
vanessacastro <vanessa-castro30@hotmail.com>
2019-06-16 23:45:35 -0600

    iniciando

---
+++ app/src/main/java/dev/grupo5/autismotion/Emociones/EmocionesActivity.java
@@ -0,0 +1,15 @@
+package dev.grupo5.autismotion.Emociones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dev.grupo5.autismotion.R;
+
+public class EmocionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emociones);
+    }
+}
