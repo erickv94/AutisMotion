4fc15338fda9923ccee095c96a62fde1d549b9a8
12Neto <1294neto@gmail.com>
2019-06-17 02:16:14 -0600

    JuegoAnimalesSonido
    Cancion en menu y juego

--- app/src/main/java/dev/grupo5/autismotion/Animales/AnimalesMenuActivity.java
+++ app/src/main/java/dev/grupo5/autismotion/Animales/AnimalesMenuActivity.java
@@ -1,21 +1,29 @@
 package dev.grupo5.autismotion.Animales;
 
+import android.app.Service;
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import dev.grupo5.autismotion.R;
 
 public class AnimalesMenuActivity extends AppCompatActivity {
 
+    MediaPlayer reproductor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animales_menu);
+        reproductor=MediaPlayer.create(getApplicationContext(), R.raw.sonidofondoanimales);
+        reproductor.start();
 
 
 
@@ -28,4 +36,26 @@ public class AnimalesMenuActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (reproductor.isPlaying()) {
+            reproductor.stop();
+            reproductor.release();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        reproductor.start();
+    }
+
+    /*@Override
+    protected void onPause() {
+        super.onPause();
+        reproductor.pause();
+    }*/
+
 }
