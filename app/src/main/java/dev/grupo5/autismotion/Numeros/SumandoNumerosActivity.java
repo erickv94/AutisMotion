ae96846f3e3c2fae701ffd29800c14a64b0705bf
ElmerHernandez <elmerh49@gmail.com>
2019-06-16 18:05:05 -0600

    sumando

--- app/src/main/java/dev/grupo5/autismotion/Numeros/SumandoNumerosActivity.java
+++ app/src/main/java/dev/grupo5/autismotion/Numeros/SumandoNumerosActivity.java
@@ -2,6 +2,7 @@ package dev.grupo5.autismotion.Numeros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -17,11 +18,17 @@ public class SumandoNumerosActivity extends AppCompatActivity {
 
     //generando numero aleatorios entre 0 y 9
     Random aleatorio = new Random(System.currentTimeMillis());
-    int num1suma=aleatorio.nextInt(10);
-    int num2suma=aleatorio.nextInt(10);
+    //int num1suma=aleatorio.nextInt(10);
+    //int num2suma=aleatorio.nextInt(10);
+    int num1suma=aleatorio.nextInt(5);
+    int num2suma=aleatorio.nextInt(5);
 
-    //posiciones para resultados
-    int posicion=aleatorio.nextInt(3);
+    //posiciones para resultados de 0 a 2
+    //int posicion=aleatorio.nextInt(3);
+    int posicion=0;
+    int resulaleat1=aleatorio.nextInt(5);
+    int resulaleat2=aleatorio.nextInt(5);
+
 
 
     int resultado =0;
@@ -37,41 +44,289 @@ public class SumandoNumerosActivity extends AppCompatActivity {
         imageNum2Sum=findViewById(R.id.imageNum2Sum);
         imageNum1ResulSuma=findViewById(R.id.imageNum1ResulSuma);
         imageNum2ResulSuma=findViewById(R.id.imageNum2ResulSuma);
-        imageNum3ResulSuma=findViewById(R.id.imageNum1ResulSuma);
+        imageNum3ResulSuma=findViewById(R.id.imageNum3ResulSuma);
         asignacionnum1(num1suma);
         asignacionnum2(num2suma);
-
-
+        resultado=num1suma+num2suma;
+        asignacionResultado(0,resultado,resulaleat1,resulaleat2);
 
     }
+   
 
-    public int sumando(int  num1suma, int num2suma){
-        resultado = num1suma+num2suma;
-        return resultado;
-    }
+    public void asignacionResultado(int posicion,int resulado,int resulaleat1, int resulaleat2){
+//para no repetir
+        while(resulaleat1==resulado){
+            resulaleat1=aleatorio.nextInt(5);
+        }
+        while(resulaleat2==resulado || resulaleat2==resulaleat1){
+            resulaleat2=aleatorio.nextInt(5);
+        }
 
-    public void asignacionresultado(int posicion){
+
         switch (posicion){
             case 0:{
-                imageNum1ResulSuma=findViewById(R.id.imageNum1ResulSuma);
+                switch (resulado){
+                    case 0:{
+                        imageNum1ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+
+
+                        switch (resulaleat1){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        switch (resulaleat2){
+                            case 0:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+
+                    case 1:{
+                        imageNum1ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                        switch (resulaleat1){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        switch (resulaleat2){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case 2:{
+                        imageNum1ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                        switch (resulaleat1){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        switch (resulaleat2){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case 3:{
+                        imageNum1ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                        switch (resulaleat1){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        switch (resulaleat2){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case 4:{
+                        imageNum1ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                        switch (resulaleat1){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        switch (resulaleat2){
+                            case 0:{
+                                imageNum2ResulSuma.setBackgroundResource(R.drawable.animal_number_0_t);
+                                break;
+                            }
+                            case 1:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_1_t);
+                                break;
+                            }
+                            case 2:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_2_t);
+                                break;
+                            }
+                            case 3:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_3_t);
+                                break;
+                            }
+                            case 4:{
+                                imageNum3ResulSuma.setBackgroundResource(R.drawable.animal_number_4_t);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+
+                }
 
                 break;
             }
+
+
             case 1:{
-                imageNum2ResulSuma=findViewById(R.id.imageNum2ResulSuma);
-
-
                 break;
             }
             case 2:{
-
-                imageNum3ResulSuma=findViewById(R.id.imageNum3ResulSuma);
-
                 break;
             }
-
         }
+
+
     }
+
 
 
 
