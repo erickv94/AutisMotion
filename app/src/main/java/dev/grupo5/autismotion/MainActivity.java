132df8bb61a542520020f1f543164f1bad127038
vanessacastro <vanessa-castro30@hotmail.com>
2019-06-17 13:53:16 -0600

    afinando layout

--- app/src/main/java/dev/grupo5/autismotion/MainActivity.java
+++ app/src/main/java/dev/grupo5/autismotion/MainActivity.java
@@ -4,20 +4,25 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends ListActivity {
+import dev.grupo5.autismotion.Emociones.EmocionesActivity;
+import dev.grupo5.autismotion.Numeros.MenuNumerosActivity;
 
-    String[] menu={"Menu Matematicas"};
+public class MainActivity extends AppCompatActivity {
 
-    String [] activities={"MenuNumerosActivity"};
+    String[] menu={"Menu Matematicas","Menu Emociones"};
+
+    String [] activities={"MenuNumerosActivity","EmocionesMenuActivity"};
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu));
+        setContentView(R.layout.activity_main);
     }
+
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id){
         super.onListItemClick(l, v, position, id);
@@ -30,8 +35,37 @@ public class MainActivity extends ListActivity {
             }catch(ClassNotFoundException e){
                 e.printStackTrace();
             }
+
+            try{
+                Class<?> clase=Class.forName("dev.grupo5.autismotion.Emociones."+nombreValue);
+                Intent inte = new Intent(this,clase);
+                this.startActivity(inte);
+            } catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
         }else{
         }
+
+
+    public void matematicaOption(View view){
+        Intent intent= new Intent(MainActivity.this, MenuNumerosActivity.class);
+        startActivity(intent);
+
+
     }
+
+    public void emocionesOption(View view){
+        Intent intent= new Intent(MainActivity.this, EmocionesActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void animalesOption(View view){
+        //cambiar activitygit
+        Intent intent= new Intent(MainActivity.this, EmocionesActivity.class);
+        startActivity(intent);
+
+    }
+
 }
 
